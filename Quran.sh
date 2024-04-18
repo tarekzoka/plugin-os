511fa23fb16c24e788c1e86e12ea5bdc85da1f61
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-04-18 05:02:22 +0200

    Add files via upload

--- Quran.sh
+++ Quran.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 #
 
-wget -O /tmp/Quran.tar.gz "https://raw.githubusercontent.com/tarekzoka/plugin-os/main/Quran.tar.gz "
+wget -O /tmp/Quran.tar.gz "https://raw.githubusercontent.com/tarekzoka/plugin-os/main/Quran.tar.gz"
 
-tar -xzf /tmp/*.tar.gz -C /
+tar -xzf /tmp/Quran.tar.gz.tar.gz -C /
 
-rm -r /tmp/Quran.tar.gz 
+rm -r /tmp/Quran.tar.gz
 
 
 killall -9 enigma2
 
 sleep 2;
-
