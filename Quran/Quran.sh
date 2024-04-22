e2a742aa9f91b412d6af578424f1593fbb139f4a
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-04-22 14:59:48 +0200

    Add files via upload

--- Quran/Quran.sh
+++ Quran/Quran.sh
@@ -13,11 +13,16 @@ wget -O /usr/lib/enigma2/python/Plugins/Extensions/Quran/plugin.py "https://gith
 wait
 wget -O /usr/lib/enigma2/python/Plugins/Extensions/Quran/plugin.pyo "https://github.com/tarekzoka/plugin-os/blob/main/Quran/plugin.pyo"
 wait
-wget -O /usr/lib/enigma2/python/Plugins/Extensions/Quran/tilawat/kuraas.xml"https://github.com/tarekzoka/plugin-os/blob/main/Quran/tilawat/kuraas.xml"
+#!/bin/sh
+#
+mkdir /usr/lib/enigma2/python/Plugins/Extensions/Quran/tilawat
+
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/Quran/tilawat/kuraas.xml"https://github.com/tarekzoka/plugin-os/raw/main/Quran/tilawat/kuraas.xml"
 wait
-wget -O /usr/lib/enigma2/python/Plugins/Extensions/Quran/tilawat/rtmpdump"https://github.com/tarekzoka/plugin-os/blob/main/Quran/tilawat/rtmpdump"
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/Quran/tilawat/rtmpdump"https://github.com/tarekzoka/plugin-os/raw/main/Quran/tilawat/rtmpdump"
 wait
-wget -O /usr/lib/enigma2/python/Plugins/Extensions/Quran/tilawat/soura.xml"https://github.com/tarekzoka/plugin-os/blob/main/Quran/tilawat/soura.xml"
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/Quran/tilawat/soura.xml"https://github.com/tarekzoka/plugin-os/raw/main/Quran/tilawat/soura.xml"
 wait
 
 #!/bin/sh
@@ -29,9 +34,30 @@ wget -O /tmp/icon.tar.gz "https://raw.githubusercontent.com/tarekzoka/plugin-os/
 tar -xzf /tmp/icon.tar.gz -C /
 
 rm -r /tmp/Quran.tar.gz
+wait
 
+#!/bin/sh
+
+#
+
+wget -O /tmp/sourse.tar.gz "https://raw.githubusercontent.com/tarekzoka/plugin-os/main/Quran/sourse.tar.gz"
+
+tar -xzf /tmp/sourse.tar.gz -C /
+
+rm -r /tmp/sourse.tar.gz
+
+wait
+
+#!/bin/sh
+
+#
+
+wget -O /tmp/quransourse.tar.gz "https://raw.githubusercontent.com/tarekzoka/plugin-os/main/Quran/quransourse.tar.gz"
+
+tar -xzf /tmp/quransourse.tar.gz -C /
+
+rm -r /tmp/quransourse.tar.gz
 
 killall -9 enigma2
 
 sleep 2;
-
