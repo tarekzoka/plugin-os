0f8b4cc0dfb4cf1fb338a92a5b651e26ac883cfc
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-04-28 16:56:26 +0300

    Add files via upload

---
+++ footonsat/footonsat.sh
@@ -0,0 +1,23 @@
+#!/bin/s
+#
+wget -O /tmp/footonsat_1.9-r0_all.deb "https://raw.githubusercontent.com/tarekzoka/plugin-os/main/footonsat/enigma2-plugin-extensions-footonsat_1.9-r0_all.deb"
+
+wait
+
+apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
+
+wait
+
+dpkg -i --force-overwrite /tmp/*.deb
+
+wait
+
+rm -r /tmp/enigma2-plugin-extensions-footonsat_1.9-r0_all.deb
+
+wait
+
+sleep 2;
+
+exit 0
+
+
